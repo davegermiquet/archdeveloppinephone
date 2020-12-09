2590a73d29ed3513bce1ff07d4806551e7314f89
root <david.germiquet@trulycanadian.net>
2020-12-08 21:57:39 -0500

    Not sure why /media didnt work lets put it in tmp

--- build_files/mount_image_and_compile.sh
+++ build_files/mount_image_and_compile.sh
@@ -4,14 +4,14 @@ sudo mknod -m 0660 "/tmp/archimage-docker-loop0" b 7 101
 cd /tmp/dir_pkg_build/imagetomount
 sudo losetup -P /tmp/archimage-docker-loop0  /tmp/dir_pkg_build/imagetomount/archlinux-pinephone-20201204.img
 sudo /tmp/dir_pkg_build/createnode.sh
-mkdir -p /media/archimage
-sudo mount /tmp/archimage-docker-loop0p1 /media/archimage
-sudo cp /usr/bin/qemu-aarch64-static /media/archimage/usr/bin/
-sudo mkdir /media/archimage/tmp/dir_pkg_build
-sudo rsync /tmp/dir_pkg_build /media/archimage/tmp/dir_pkg_build
-sudo chown -R aurpackageadd /media/archimage/tmp/dir_pkg_build
+mkdir -p /tmp/mountedarchimage
+sudo mount /tmp/archimage-docker-loop0p1 /tmp/mountedarchimage
+sudo cp /usr/bin/qemu-aarch64-static /tmp/mountedarchimage/usr/bin/
+sudo mkdir -p /tmp/mountedarchimage/tmp/dir_pkg_build
+sudo rsync /tmp/dir_pkg_build /tmp/mountedarchimage/tmp/dir_pkg_build
 sudo chown -R aurpackageadd /tmp/dir_pkg_build
-sudo -u aurpackageadd sh -c "chroot /media/archimage/usr/bin/;source /tmp/environment.func;cd /tmp/dir_pkg_build;setup_distcc;makepkg -s --noconfirm"
+sudo chown -R aurpackageadd /tmp/mountedarchimage/tmp/dir_pkg_build
+sudo -u aurpackageadd sh -c "chroot /tmp/mountedarchimage /usr/bin/bash;source /tmp/environment.func;cd /tmp/dir_pkg_build;setup_distcc;makepkg -s --noconfirm"
 
 
 
