92d94e4d2fdc0ea571a5041d23c6bfcb80581b5f
root <david.germiquet@trulycanadian.net>
2020-12-09 09:03:32 -0500

    Update for people who wanna see what im doing

--- build_files/mount_image_and_compile.sh
+++ build_files/mount_image_and_compile.sh
@@ -3,6 +3,7 @@
 sudo mknod -m 0660 "/tmp/archimage-docker-loop0" b 7 101
 cd /tmp/dir_pkg_build/imagetomount
 sudo losetup -P /tmp/archimage-docker-loop0  /tmp/dir_pkg_build/imagetomount/archlinux-pinephone-20201204.img
+sudo resize.f2fs /tmp/archimage-docker-loop0p1
 sudo /tmp/dir_pkg_build/createnode.sh
 sudo mkdir -p /tmp/mountedarchimage
 sudo mount /tmp/archimage-docker-loop0p1 /tmp/mountedarchimage
@@ -10,6 +11,13 @@ sudo cp /usr/bin/qemu-aarch64-static /tmp/mountedarchimage/usr/bin/
 sudo mkdir -p /tmp/mountedarchimage/tmp/dir_pkg_build
 sudo cp /tmp/dir_pkg_build/setup_distcc.sh /tmp/mountedarchimage/tmp/dir_pkg_build
 sudo cp /tmp/dir_pkg_build/PKGBUILD /tmp/mountedarchimage/tmp/dir_pkg_build/
+
+sudo mount -t proc /proc  /tmp/mountedarchimage/proc/
+sudo mount --rbind /sys  /tmp/mountedarchimage/sys/
+sudo mount --rbind /dev  /tmp/mountedarchimage/dev/
+sudo mount --rbind /run /tmp/mountedarchimage/run/
+sudo cp /etc/resolv.conf /tmp/mountedarchimage/etc/resolv.conf
+
 sudo chroot /tmp/mountedarchimage sh -c 'cd /tmp/dir_pkg_build; \
 ./setup_distcc.sh; \
 sudo -u alarm "export DISTCC_HOSTS=\"192.168.1.183/4 192.168.1.184/4\"; \
