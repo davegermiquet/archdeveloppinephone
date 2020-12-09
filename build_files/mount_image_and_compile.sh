1dabb6754f9c2ce61e699dbecd1cd646ed752ad0
root <david.germiquet@trulycanadian.net>
2020-12-09 10:57:56 -0500

    Sometimes you forget things...like when you cut and paste a folder rename

--- build_files/mount_image_and_compile.sh
+++ build_files/mount_image_and_compile.sh
@@ -3,25 +3,28 @@
 sudo mknod -m 0660 "/tmp/archimage-docker-loop0" b 7 101
 cd /tmp/dir_pkg_build/imagetomount
 sudo losetup -P /tmp/archimage-docker-loop0  /tmp/dir_pkg_build/imagetomount/archlinux-pinephone-20201204.img
-sudo parted /tmp/archimage-docker-loop0 resizepart 1 50G
-
 sudo /tmp/dir_pkg_build/createnode.sh
-sudo resize.f2fs /tmp/archimage-docker-loop0p1
 
 sudo mkdir -p /tmp/mountedarchimage
 sudo mount /tmp/archimage-docker-loop0p1 /tmp/mountedarchimage
-sudo cp /usr/bin/qemu-aarch64-static /tmp/mountedarchimage/usr/bin/
-sudo mkdir -p /tmp/mountedarchimage/tmp/dir_pkg_build
-sudo cp /tmp/dir_pkg_build/setup_distcc.sh /tmp/mountedarchimage/tmp/dir_pkg_build
-sudo cp /tmp/dir_pkg_build/PKGBUILD /tmp/mountedarchimage/tmp/dir_pkg_build/
+sudo mkdir /tmp/rootdrive
+sudo rsync -avh /tmp/mountedarchimage/ /tmp/rootdrive/
 
-sudo mount -t proc /proc  /tmp/mountedarchimage/proc/
-sudo mount --rbind /sys  /tmp/mountedarchimage/sys/
-sudo mount --rbind /dev  /tmp/mountedarchimage/dev/
-sudo mount --rbind /run /tmp/mountedarchimage/run/
-sudo cp /etc/resolv.conf /tmp/mountedarchimage/etc/resolv.conf
+ls -la /tmp/rootdrive
 
-sudo chroot /tmp/mountedarchimage sh -c 'cd /tmp/dir_pkg_build; \
+sudo cp /usr/bin/qemu-aarch64-static /tmp/rootdrive/usr/bin/
+sudo mkdir -p /tmp/rootdrive/tmp/dir_pkg_build
+
+sudo cp /tmp/dir_pkg_build/setup_distcc.sh /tmp/rootdrive/tmp/dir_pkg_build
+sudo cp /tmp/dir_pkg_build/PKGBUILD /tmp/rootdrive/tmp/dir_pkg_build/
+
+sudo mount -t proc /proc  /tmp/rootdrive/proc/
+sudo mount --rbind /sys  /tmp/rootdrive/sys/
+sudo mount --rbind /dev /tmp/rootdrive//dev/
+sudo mount --rbind /run /tmp/rootdrive/run/
+sudo cp /etc/resolv.conf /tmp/rootdrive/etc/resolv.conf
+
+sudo chroot /tmp/rootdrive/sh -c 'cd /tmp/dir_pkg_build; \
 ./setup_distcc.sh; \
 sudo -u alarm "export DISTCC_HOSTS=\"192.168.1.183/4 192.168.1.184/4\"; \
 export CCACHE_DIR=/root/.ccache;export PATH=\"/usr/lib/distcc/:$PATH\"; \
