5f35d0dfc46d24b2f9caed561030dfc996df5d18
root <david.germiquet@trulycanadian.net>
2020-12-09 12:53:04 -0500

    Change to Docker-Compose!

---
+++ distccmastercompilation/build_files/scripts/mount_image_and_compile.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+sudo mknod -m 0660 "/tmp/archimage-docker-loop0" b 7 101
+
+cd /tmp/setup/mount
+
+sudo losetup -P /tmp/archimage-docker-loop0  /tmp/setup/pinephonearch.img
+sudo /tmp/setup/scripts/createnode.sh
+
+sudo mkdir -p /tmp/mountedarchimage
+sudo mount /tmp/archimage-docker-loop0p1 /tmp/image_attached
+sudo mkdir /tmp/modified_image
+sudo rsync -avh /tmp/image_attached /tmp/modified_image
+
+sudo cp /usr/bin/qemu-aarch64-static /tmp/modified_image/usr/bin/
+
+sudo mkdir -p /tmp/modified_image/tmp/setup/
+
+sudo cp /tmp/setup/setup_distcc.sh /tmp/modified_image/tmp/setup/scripts/
+
+sudo cp /tmp/setup/PKGBUILD /tmp/modified_image/tmp/setup/
+
+sudo mount -t proc /proc /tmp/modified_image/proc
+sudo mount --rbind /sys /tmp/modified_image/sys
+sudo mount --rbind /dev /tmp/modified_image/dev
+sudo mount --rbind /run /tmp/modified_image/run
+
+sudo cp /etc/resolv.conf /tmp/modified_image/etc/resolv.conf
+
+sudo chroot /tmp/modified_image/ -c 'cd /tmp/setup/scripts; \
+./setup_distcc.sh; \
+sudo -u alarm "export DISTCC_HOSTS=\"192.168.1.183/4 192.168.1.184/4\"; \
+export CCACHE_DIR=/root/.ccache;export PATH=\"/usr/lib/distcc/:$PATH\"; \
+makepkg -s --noconfirm"'
+
+
+
+
+
+
+
---
+++ distccmastercompilation/build_files/scripts/setup_distcc.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+pacman -Syu --needed --noconfirm autoconf automake binutils bison fakeroot file findutils  flex  gawk  gcc gettext  grep  groff  gzip  libtool m4  make  pacman patch  pkgconf sed sudo texinfo which cmake sdl2 glew-wayland glew1.10 libglvnd openssl sqlite3 gtk2 zlib nasm gxmessage kdialog fluidsynth sdl sdl2 extra/glu f2fs-tools xz qemu-arch-extra distcc git rsync  cmake sdl2 glew-wayland glew1.10 libglvnd openssl sqlite3 gtk2 zlib nasm gxmessage kdialog fluidsynth sdl sdl2 glu
+sudo -u alarm sh -c "mkdir -p /tmp/addPackage \
+cd /tmp/addPackage \
+git clone https://aur.archlinux.org/distccd-alarm.git \
+cd /tmp/addPackage/distccd-alarm \
+makepkg -si --noconfirm"
+
+echo "192.168.1.183/4 192.168.1.184/4" > /etc/distcc/hosts
+
+COMPILERS_TO_REPLACE=$(ls /usr/lib/distcc/ | grep -v ccache )
+COMPILERS_TO_REPLACE=" cc c++"
+
+for bin in ; do
+  rm /usr/lib/distcc/
+done
+
+  # Create distcc wrapper
+echo "#!/usr/bin/env bash" > /usr/lib/distcc/distccwrapper
+echo "export CCACHE_PREFIX=distcc" >> /usr/lib/distcc/distccwrapper
+echo "export PATH=/usr/lib/ccache/:$PATH" >> /usr/lib/distcc/distccwrapper
+echo "PATH=/usr/bin:$PATH /usr/lib/ccache/$(basename ${0}) $@" >> /usr/lib/distcc/distccwrapper
+
+chmod +x /usr/lib/distcc/distccwrapper
+
+# Create distcc wrapper
+for bin in ; do
+    ln -s /usr/lib/distcc/distccwrapper /usr/lib/distcc/
+done
+
+mkdir -p /usr/lib/ccache/
+
+for bin in ; do
+    ln -s /usr/bin/ccache  /usr/lib/ccache/${bin}
+done
+
+export DISTCC_HOSTS="192.168.1.183/4 192.168.1.184/4"
+export CCACHE_DIR=/root/.ccache
+export PATH="/usr/lib/distcc/:$PATH"
