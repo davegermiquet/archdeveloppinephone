c5dc4039413664b4cabf7ec88a5ec9ce538c5a12
root <david.germiquet@trulycanadian.net>
2020-12-10 08:44:41 -0500

    Mostly done, one needed fix, only compiles doesn't reinstall image everytime

--- distccmastercompilation/build_files/scripts/mount_image_and_compile.sh
+++ distccmastercompilation/build_files/scripts/mount_image_and_compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [[ ! -f /tmp/modified_image/run ]] ; then
+
 sudo mknod -m 0660 "/tmp/archimage-docker-loop0" b 7 101
 
 cd /tmp/setup/
@@ -11,28 +13,29 @@ sudo mkdir -p /tmp/image_attached
 sudo mkdir -p /tmp/modified_image
 
 sudo mount /tmp/archimage-docker-loop0p1 /tmp/image_attached	
-sudo rsync -avh /tmp/image_attached/* /tmp/modified_image/
-
+sudo rsync -avh /tmp/image_attached/ /tmp/modified_image/
 sudo cp /usr/bin/qemu-aarch64-static /tmp/modified_image/usr/bin/
 
 sudo mkdir -p /tmp/modified_image/tmp/setup/scripts
-
-sudo cp /tmp/setup/scripts/setup_distcc.sh /tmp/modified_image/tmp/setup/scripts/
-sudo cp /tmp/setup/GameMusicCompilerPtch.patch /tmp/modified_image/tmp/setup/GameCompiler.patch
-sudo cp /tmp/setup/PKGBUILD /tmp/modified_image/tmp/setup/
 
 sudo mount -t proc /proc /tmp/modified_image/proc
 sudo mount --rbind /sys /tmp/modified_image/sys
 sudo mount --rbind /dev /tmp/modified_image/dev
 sudo mount --rbind /run /tmp/modified_image/run
+fi
+
+# Setup Done All Above is only done once Refresh Files 
+
+
+sudo cp /tmp/setup/scripts/setup_distcc.sh /tmp/modified_image/tmp/setup/scripts/
+sudo cp /tmp/setup/GameMusicCompilerPtch.patch /tmp/modified_image/tmp/setup/GameCompiler.patch
+sudo cp /tmp/setup/PKGBUILD /tmp/modified_image/tmp/setup/
 sudo cp /etc/resolv.conf /tmp/modified_image/etc/resolv.conf
+
 sudo chroot /tmp/modified_image/ sh -c 'cd /tmp/setup/; \
 scripts/setup_distcc.sh; \
-sudo chown -R alarm /tmp/; \
-sudo -u alarm sh -c "export DISTCC_HOSTS=\"192.168.1.183/4 192.168.1.184/4\"; \
-export CCACHE_DIR=/root/.ccache;export PATH=\"/usr/lib/distcc/:$PATH\"; 
-ls -la /tmp/setup;\
-makepkg -s --noconfirm"'
+chown -R alarm /tmp/setup
+sudo -u alarm  PATH=$PATH:/usr/lib/distcc:/usr/bin:/usr/sbin:/bin:/sbin makepkg -s --noconfirm'
 cat /tmp/modified_image/tmp/setup/src/zandronum/build/CMakeFiles/CMakeError.log
 mkdir /tmp/setup/release
 cp /tmp/modified_image/tmp/setup/*.zst /tmp/setup/release
--- distccmastercompilation/build_files/scripts/setup_distcc.sh
+++ distccmastercompilation/build_files/scripts/setup_distcc.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
-pacman -Syu --needed --noconfirm autoconf automake binutils bison fakeroot file findutils  flex  gawk  gcc gettext  grep  groff  gzip  libtool m4  make  pacman patch pkgconf sed sudo texinfo which cmake sdl2 glew-wayland glew1.10 libglvnd openssl sqlite3 gtk2 zlib nasm gxmessage fluidsynth sdl sdl2 extra/glu f2fs-tools xz distcc git rsync  cmake sdl2 glew-wayland glew1.10 libglvnd openssl sqlite3 gtk2 zlib nasm ccache gxmessage kdialog fluidsynth sdl sdl2 patch glu
+pacman -Syu --needed --noconfirm base-devel autoconf automake binutils bison fakeroot file findutils  flex  gawk  gcc gettext  grep  groff  gzip  libtool m4  make  pacman patch pkgconf sed sudo texinfo which cmake sdl2 glew-wayland glew1.10 libglvnd openssl sqlite3 gtk2 zlib nasm gxmessage fluidsynth sdl sdl2 extra/glu f2fs-tools xz distcc git rsync  cmake sdl2 glew-wayland glew1.10 libglvnd openssl sqlite3 gtk2 zlib nasm ccache gxmessage kdialog fluidsynth sdl sdl2 patch glu
 
 echo "192.168.1.183/4 192.168.1.184/4" > /etc/distcc/hosts
 
-COMPILERS_TO_REPLACE=$(ls /usr/lib/distcc/ | grep -v ccache )
-COMPILERS_TO_REPLACE=" cc c++"
+COMPILERS_TO_REPLACE=$(ls /usr/lib/ccache/bin)	
 
-for bin in ; do
-  rm /usr/lib/distcc/
+for bin in ${COMPILERS_TO_REPLACE}; do
+  rm /usr/lib/distcc/${bin}
+done
+
+
+for bin in ${COMPILERS_TO_REPLACE}; do
+  rm /usr/lib/distcc/bin/${bin}
 done
 
   # Create distcc wrapper
-echo "#!/usr/bin/env bash" > /usr/lib/distcc/distccwrapper
+echo "#!/usr/sbin/env bash" > /usr/lib/distcc/distccwrapper
 echo "export CCACHE_PREFIX=distcc" >> /usr/lib/distcc/distccwrapper
-echo "export PATH=/usr/lib/ccache/:$PATH" >> /usr/lib/distcc/distccwrapper
-echo "PATH=/usr/bin:$PATH /usr/lib/ccache/$(basename ${0}) $@" >> /usr/lib/distcc/distccwrapper
+echo "export PATH=/usr/lib/ccache/bin/:\$PATH" >> /usr/lib/distcc/distccwrapper
+echo "export PATH=/usr/lib/distcc:\$PATH" >> /usr/lib/distcc/distccwrapper
+echo "PATH=/usr/bin:$PATH /usr/lib/ccache/bin/\$(basename \${0}) \$@" >> /usr/lib/distcc/distccwrapper
 
 chmod +x /usr/lib/distcc/distccwrapper
 
-# Create distcc wrapper
-for bin in ; do
-    ln -s /usr/lib/distcc/distccwrapper /usr/lib/distcc/
+
+for bin in ${COMPILERS_TO_REPLACE}; do
+  ln -s /usr/lib/distcc/distccwrapper /usr/lib/distcc/bin/${bin}
 done
 
-mkdir -p /usr/lib/ccache/
+for bin in ${COMPILERS_TO_REPLACE}; do
+  ln -s /usr/lib/distcc/distccwrapper /usr/lib/distcc/${bin}
+done
 
-for bin in ; do
-    ln -s /usr/bin/ccache  /usr/lib/ccache/${bin}
+
+for bin in ${COMPILERS_TO_REPLACE}; do
+  rm /usr/lib/ccache/bin/${bin}
+done
+
+for bin in ${COMPILERS_TO_REPLACE}; do
+    ln -s /usr/bin/ccache  /usr/lib/ccache/bin/${bin}
 done
 
 export DISTCC_HOSTS="192.168.1.183/4 192.168.1.184/4"
-export CCACHE_DIR=/root/.ccache
 export PATH="/usr/lib/distcc/:$PATH"
-cat  /usr/lib/distcc/distccwrapper
+
